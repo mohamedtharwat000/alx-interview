853778360e5906a836089218653e6ea23342ec07
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-01 01:09:59 +0200

    Create 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""Fewest number of coins needed to meet a given amount."""
+
+def make_change(coins, amount):
+    """Return the fewest number of coins needed to meet a given amount."""
+    if amount == 0:
+        return 0
+    coins.sort(reverse=True)
+    result = []
+    for coin in coins:
+        while coin <= amount:
+            result.append(coin)
+            amount -= coin
+    return len(result)
