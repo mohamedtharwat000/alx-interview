7ff9573d8d2fce9b4bbdae66443db5fa2568162d
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-08 02:05:48 +0200

    Refactor island_perimeter function to use enumerate and continue statement

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,15 +6,16 @@
 def island_perimeter(grid):
     """returns the perimeter of the island"""
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            if i == 0 or grid[i - 1][j] == 0:
+                perimeter += 1
+            if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                perimeter += 1
+            if j == 0 or grid[i][j - 1] == 0:
+                perimeter += 1
+            if j == len(row) - 1 or grid[i][j + 1] == 0:
+                perimeter += 1
     return perimeter
